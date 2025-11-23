855e467a6775bb3c629ce6e044be88c1108af963
Andriy Symonenko Oliynyk <andriysymonenko@gmail.com>
2025-11-23 16:15:17 +0100

    Usar Faker\Factory::create() directamente en UserFactory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,9 +24,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = FakerFactory::create();
         return [
-            'name' => Factory::faker()->name(),
-            'email' => Factory::faker()->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
